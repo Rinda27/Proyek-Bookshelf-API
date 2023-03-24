6ac3f6763259f4eb921d80c3840a632f7345ea5d
Rinda Ismawati <rindaismawaty@gmail.com>
2023-03-25 00:15:07 +0700

    initial commit

--- src/server.js
+++ src/server.js
@@ -6,16 +6,15 @@ const init = async () => {
     port: 9000,
     host: 'localhost',
     routes: {
-        cors: {
-          origin: ['*'],
-        },
-      }, 
-    });
-   
-    server.route(routes);
-  
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
-  };
-   
-  init();
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
